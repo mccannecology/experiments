aca2f04f064be03b1665eff9e776c083957c43c1
mccannecology <mccannmikejames@gmail.com>
2014-03-17 09:47:15 -0400

    importaing & some basic plots of the raw #s

--- multispecies.R
+++ multispecies.R
@@ -1,14 +1,36 @@
 ################################################
 # Analysis of multispecies duckweed experiment #
 ################################################
+library(ggplot2)
+library(plyr)
+
+###################################### 
+# Importing & cleaning up data frame #
+######################################
+data_area <- read.csv("multispecies_area.csv") # import area data 
+data_rgr <- read.csv("multispecies_rgr.csv") # import relative growth rate data 
+
+data_area <- data_area[complete.cases(data_area),]
+data_rgr <- data_rgr[complete.cases(data_rgr),]
+
+summary(data_area)
+summary(data_rgr)
+
+#######################
+# Means and variances #
+#######################
 
 
-############# 
-# Importing #
-#############
+##################################
+# Plot raw rgr data through time #
+##################################
+rgr_plot_raw <- ggplot(data_rgr, aes(x=day,y=rgr,colour=species)) + geom_point() 
+rgr_plot_raw <- rgr_plot_raw + facet_grid(nutrients ~ treatment)
+rgr_plot_raw 
 
-data <- read.csv("multispecies data 03-16-2014.csv",header=F) # import data matrix - do not include a header row 
-
-library(reshape)
-
-newdata <- melt(mydata, id=...)
+###################################
+# Plot raw area data through time #
+##################################
+area_plot_raw <- ggplot(data_area, aes(x=day,y=area_mm2,colour=species)) + geom_point() 
+area_plot_raw <- area_plot_raw + facet_grid(nutrients ~ treatment)
+area_plot_raw 
