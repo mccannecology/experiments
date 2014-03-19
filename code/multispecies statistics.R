a85af38ac10f0f20de46128376096694c767f723
mccannecology <mccannmikejames@gmail.com>
2014-03-19 17:07:56 -0400

    added anova for each sp. rgr ~ nutr. * treat (both min and max rgr separately)

--- code/multispecies statistics.R
+++ code/multispecies statistics.R
@@ -5,7 +5,7 @@
 ################################################
 
 ###############################
-# Test parametric assumptions #
+# Test parametric assumptions # ################# This should go with the analysis that it accompanies 
 ###############################
 nutrients<-c("low","high")
 treatment<-c("LM","SP","WB","LMSP","LMWB","SPWB","LMSPWB")
@@ -34,7 +34,6 @@ for (i in 1:length(nutrients)){
 # Y= total standardized area #
 # Nutrients * sp. treatment  #
 ##############################
-
 # subset your data first 
 data_area_10 <- subset(data_area, data_area$day == 10)
 data_area_10_TOT <- subset(data_area_10, data_area_10$species == "TOT")
@@ -46,10 +45,66 @@ posthoc_anova_area_stand_10_TOT <- posthoc_anova_area_stand_10_TOT$'nutrients:tr
 significant <- subset(posthoc_anova_area_stand_10_TOT, posthoc_anova_area_stand_10_TOT[,4]<=0.050) # significant comparisons 
 nonsignif <- subset(posthoc_anova_area_stand_10_TOT, posthoc_anova_area_stand_10_TOT[,4]>0.050) # non-significant comparisons 
 
+##############################
+# Two-way anova              #
+# Y= max RGR                 #
+# Lemna                      #
+# Nutrients * sp. treatment  #
+##############################
+data_rgr_LM <- subset(summary_data_rgr02, summary_data_rgr02$species == "LM")
+anova_rgr_max_LM <- aov(max ~ nutrients * treatment, data=data_rgr_LM)
+summary(anova_rgr_max_LM)
+
+##############################
+# Two-way anova              #
+# Y= avg RGR                 #
+# Lemna                      #
+# Nutrients * sp. treatment  #
+##############################
+anova_rgr_mean_LM <- aov(mean ~ nutrients * treatment, data=data_rgr_LM)
+summary(anova_rgr_mean_LM)
+
+##############################
+# Two-way anova              #
+# Y= max RGR                 #
+# Wolffia                    #
+# Nutrients * sp. treatment  #
+##############################
+data_rgr_WB <- subset(summary_data_rgr02, summary_data_rgr02$species == "WB")
+anova_rgr_max_WB <- aov(max ~ nutrients * treatment, data=data_rgr_WB)
+summary(anova_rgr_max_WB)
+
+##############################
+# Two-way anova              #
+# Y= avg RGR                 #
+# Wolffia                    #
+# Nutrients * sp. treatment  #
+##############################
+anova_rgr_mean_WB <- aov(mean ~ nutrients * treatment, data=data_rgr_WB)
+summary(anova_rgr_mean_WB)
+
+##############################
+# Two-way anova              #
+# Y= max RGR                 #
+# Spirodela                  #
+# Nutrients * sp. treatment  #
+##############################
+data_rgr_SP <- subset(summary_data_rgr02, summary_data_rgr02$species == "SP")
+anova_rgr_max_SP <- aov(max ~ nutrients * treatment, data=data_rgr_SP)
+summary(anova_rgr_max_SP)
+
+##############################
+# Two-way anova              #
+# Y= avg RGR                 #
+# Spirodela                  #
+# Nutrients * sp. treatment  #
+##############################
+anova_rgr_mean_SP <- aov(mean ~ nutrients * treatment, data=data_rgr_SP)
+summary(anova_rgr_mean_SP)
+
+
 
 # Response: Relative growth rates 
-# Within each species, is average average RGR different betweeen species*nutrient treatments? (ANOVA)
-# Within each species, is average maximum RGR different betweeen species*nutrient treatments? (ANOVA)
 # Is the average density-specific RGR different different between species?  
 
 
