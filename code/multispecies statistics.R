7944fca64c365d30ab81a34e0e0ff93298457b5c
mccannecology <mccannmikejames@gmail.com>
2014-03-19 07:23:59 -0400

    writing out questions that I want to analyze

---
+++ code/multispecies statistics.R
@@ -0,0 +1,9 @@
+
+# Response: Area 
+# Is final (day 10) standardized area different between species*nutrient treatments? (ANOVA)
+# Is final (day 10) standardized area different between species richnesses? (harder to do this - how do I group? average?)
+
+# Response: Relative growth rates 
+# Within each species, is average average RGR different betweeen species*nutrient treatments? (ANOVA)
+# Within each species, is average maximum RGR different betweeen species*nutrient treatments? (ANOVA)
+# Is the average density-specific RGR different different between species?  
