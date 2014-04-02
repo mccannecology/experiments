03928df806d16ba29e9796c2493f6bc6751e8846
mccannecology <mccannmikejames@gmail.com>
2014-04-02 07:52:14 -0400

    added posthoc test labels to rgr plots

--- code/multspecies plot.R
+++ code/multspecies plot.R
@@ -346,7 +346,13 @@ Fig3temp
 head(summary_data_maxrgr) 
 
 # add post-hoc test labels to data frame (summary_data_maxrgr) for plotting posthoc test labels
-
+# import posthoc label data 
+rgr_max_posthoc <- read.csv("rgr_max_posthoc.csv") 
+# sort both data frames first
+summary_data_maxrgr <- summary_data_maxrgr[order(summary_data_maxrgr$maximum_RGR),]
+rgr_max_posthoc <- rgr_max_posthoc[order(rgr_max_posthoc$maximum_RGR),]
+# add the labels column
+summary_data_maxrgr$label <- rgr_max_posthoc$label
 
 # LEMNA
 maxrgr_plot_LM <- ggplot(subset(summary_data_maxrgr, summary_data_maxrgr$species=="LM"), aes(x=treatment, y=maximum_RGR,shape=nutrients)) + geom_errorbar(aes(ymin=maximum_RGR-se, ymax=maximum_RGR+se), width=0.1)
@@ -356,6 +362,7 @@ maxrgr_plot_LM <- maxrgr_plot_LM + ylim(0,0.4)
 maxrgr_plot_LM <- maxrgr_plot_LM + ggtitle("Lemna minor")
 maxrgr_plot_LM <- maxrgr_plot_LM + theme_classic()
 maxrgr_plot_LM <- maxrgr_plot_LM + theme(legend.position = "none") 
+maxrgr_plot_LM <- maxrgr_plot_LM + geom_text(data=subset(summary_data_maxrgr, summary_data_maxrgr$species=="LM"),aes(x=treatment, y=maximum_RGR,label=label),hjust=2)
 maxrgr_plot_LM
 
 # SPIRODELA
@@ -366,6 +373,7 @@ maxrgr_plot_SP <- maxrgr_plot_SP + ylim(0,0.4)
 maxrgr_plot_SP <- maxrgr_plot_SP + ggtitle("Spirodela polyrhiza")
 maxrgr_plot_SP <- maxrgr_plot_SP + theme_classic()
 maxrgr_plot_SP <- maxrgr_plot_SP + theme(legend.position = "none") 
+maxrgr_plot_SP <- maxrgr_plot_SP + geom_text(data=subset(summary_data_maxrgr, summary_data_maxrgr$species=="SP"),aes(x=treatment, y=maximum_RGR,label=label),hjust=2)
 maxrgr_plot_SP 
 
 # WOLLFIA
@@ -375,6 +383,7 @@ maxrgr_plot_WB <- maxrgr_plot_WB + ylab("maximum RGR (day 0-10)")
 maxrgr_plot_WB <- maxrgr_plot_WB + ylim(0,0.4)
 maxrgr_plot_WB <- maxrgr_plot_WB + ggtitle("Wolffia brasiliensis")
 maxrgr_plot_WB <- maxrgr_plot_WB + theme_classic()
+maxrgr_plot_WB <- maxrgr_plot_WB + geom_text(data=subset(summary_data_maxrgr, summary_data_maxrgr$species=="WB"),aes(x=treatment, y=maximum_RGR,label=label),hjust=2)
 maxrgr_plot_WB 
 
 Fig3temp <- arrangeGrob(maxrgr_plot_LM,maxrgr_plot_SP,maxrgr_plot_WB,ncol=3,nrow=1) 
@@ -439,13 +448,24 @@ Fig2temp
 ######################################################################################
 head(summary_data_meanrgr) 
 
+# add post-hoc test labels to data frame (summary_data_maxrgr) for plotting posthoc test labels
+# import posthoc label data 
+rgr_avg_posthoc <- read.csv("rgr_avg_posthoc.csv") 
+# sort both data frames first
+summary_data_meanrgr <- summary_data_meanrgr[order(summary_data_meanrgr$average_RGR),]
+rgr_avg_posthoc <- rgr_avg_posthoc[order(rgr_avg_posthoc$maximum_RGR),]
+# add the labels column
+summary_data_meanrgr$label <- rgr_avg_posthoc$label
+
 # LEMNA
 avgrgr_plot_LM <- ggplot(subset(summary_data_meanrgr, summary_data_meanrgr$species=="LM"), aes(x=treatment, y=average_RGR, shape=nutrients)) + geom_errorbar(aes(ymin=average_RGR-se, ymax=average_RGR+se), width=0.1)
 avgrgr_plot_LM <- avgrgr_plot_LM + geom_point(size=3)
 avgrgr_plot_LM <- avgrgr_plot_LM + ylim(-0.05,0.25)
 avgrgr_plot_LM <- avgrgr_plot_LM + ylab("average RGR (day 0-10)")
 avgrgr_plot_LM <- avgrgr_plot_LM + ggtitle("Lemna minor")
+avgrgr_plot_LM <- avgrgr_plot_LM + theme_classic()
 avgrgr_plot_LM <- avgrgr_plot_LM + theme(legend.position = "none") 
+avgrgr_plot_LM <- avgrgr_plot_LM + geom_text(data=subset(summary_data_meanrgr, summary_data_meanrgr$species=="LM"),aes(x=treatment, y=average_RGR,label=label),hjust=2)
 avgrgr_plot_LM 
 
 # SPIRODELA
@@ -454,7 +474,9 @@ avgrgr_plot_SP <- avgrgr_plot_SP + geom_point(size=3)
 avgrgr_plot_SP <- avgrgr_plot_SP + ylim(-0.05,0.25)
 avgrgr_plot_SP <- avgrgr_plot_SP + ylab("average RGR (day 0-10)")
 avgrgr_plot_SP <- avgrgr_plot_SP + ggtitle("Spirodela polyrhiza")
+avgrgr_plot_SP <- avgrgr_plot_SP + theme_classic()
 avgrgr_plot_SP <- avgrgr_plot_SP + theme(legend.position = "none") 
+avgrgr_plot_SP <- avgrgr_plot_SP + geom_text(data=subset(summary_data_meanrgr, summary_data_meanrgr$species=="SP"),aes(x=treatment, y=average_RGR,label=label),hjust=2)
 avgrgr_plot_SP 
 
 # WOLLFIA
@@ -463,9 +485,11 @@ avgrgr_plot_WB <- avgrgr_plot_WB + geom_point(size=3)
 avgrgr_plot_WB <- avgrgr_plot_WB + ylim(-0.05,0.25)
 avgrgr_plot_WB <- avgrgr_plot_WB + ylab("average RGR (day 0-10)")
 avgrgr_plot_WB <- avgrgr_plot_WB + ggtitle("Wolffia brasiliensis")
+avgrgr_plot_WB <- avgrgr_plot_WB + theme_classic()
+avgrgr_plot_WB <- avgrgr_plot_WB + geom_text(data=subset(summary_data_meanrgr, summary_data_meanrgr$species=="WB"),aes(x=treatment, y=average_RGR,label=label),hjust=2)
 avgrgr_plot_WB 
 
-# combine thwo plots into one
+# combine the plots into one
 Fig2temp <- arrangeGrob(avgrgr_plot_LM,avgrgr_plot_SP,avgrgr_plot_WB,ncol=3,nrow=1) 
 Fig2temp
 
