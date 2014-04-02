057d058dc1c3f01930d436758b4fa5fc9a0f17e5
mccannecology <mccannmikejames@gmail.com>
2014-04-02 08:48:39 -0400

    added nicely aligned posthoc labels

--- code/multspecies plot - area_stand day 10.R
+++ code/multspecies plot - area_stand day 10.R
@@ -15,6 +15,15 @@ library(gridExtra)
 # subset some data for this plot 
 summary_data_area_stand_10_TOT <- subset(summary_data_area_stand, summary_data_area_stand$day==10 & summary_data_area_stand$species=="TOT")
 
+# add post-hoc test labels to data frame (summary_data_maxrgr) for plotting posthoc test labels
+# import posthoc label data 
+area_stand_10_posthoc <- read.csv("area_stand_posthoc.csv") 
+# sort both data frames first
+summary_data_area_stand_10_TOT <- summary_data_area_stand_10_TOT[order(summary_data_area_stand_10_TOT$area_stand),]
+area_stand_10_posthoc <- area_stand_10_posthoc[order(area_stand_10_posthoc$area_stand),]
+# add the labels column
+summary_data_area_stand_10_TOT$label <- area_stand_10_posthoc$label
+
 # re-order my treatments so they go from monocultures to polycultures in alphabetical order 
 summary_data_area_stand_10_TOT$treatment <- factor(summary_data_area_stand_10_TOT$treatment, levels=c("LM","SP","WB","LMSP","LMWB","SPWB","LMSPWB"))
 
@@ -24,6 +33,8 @@ area_stand_plot_10 <- area_stand_plot_10 + geom_point(size=3)
 area_stand_plot_10 <- area_stand_plot_10 + ylab("final area / initial area")
 area_stand_plot_10 <- area_stand_plot_10 + xlab("species treatment")
 area_stand_plot_10 <- area_stand_plot_10 + labs(colour="Nutrients")
+area_stand_plot_10 <- area_stand_plot_10 + geom_text(data=summary_data_area_stand_10_TOT,aes(x=treatment, y=area_stand,label=label),hjust=2)
+area_stand_plot_10 <- area_stand_plot_10 + geom_text(data=summary_data_area_stand_10_TOT,aes(x=treatment, y=area_stand+se+0.5,label=label))
 area_stand_plot_10
 
 # black & white 
@@ -33,6 +44,6 @@ area_stand_plot_10 <- area_stand_plot_10 + ylab("Final area / Initial area")
 area_stand_plot_10 <- area_stand_plot_10 + xlab("Species treatment")
 area_stand_plot_10 <- area_stand_plot_10 + theme_classic(base_size=18)
 area_stand_plot_10 <- area_stand_plot_10 + labs(colour="Nutrients")
+area_stand_plot_10 <- area_stand_plot_10 + geom_text(data=summary_data_area_stand_10_TOT,aes(x=treatment, y=area_stand+se+0.5,label=label))
 area_stand_plot_10
-
 
